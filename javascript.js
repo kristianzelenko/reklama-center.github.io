814271720e4ed2b8dd1dddecc304a3bf9abbdbf4
muzytoto <77212838+muzytoto@users.noreply.github.com>
2021-03-17 15:30:47 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,51 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+$(document).ready(function () {
+  $("a").click(function () {
+      var elementClick = $(this).attr("href");
+      var destination = $(elementClick).offset().top;
+      if ($.browser.safari) {
+          $('body').animate({ scrollTop: destination }, 1100); //1100 - скорость
+      } else {
+          $('html').animate({ scrollTop: destination }, 1100);
+      }
+      return false; 
+  });
+});
+
+let menu_button = document.querySelector('.header__burger');
+let menu_itself = document.querySelector('.header__menu');
+let menu_list = document.querySelector('.header__list');
+let body = document.querySelector('body');
+
+menu_button.onclick = function() {
+  menu_button.classList.toggle('active');
+  menu_itself.classList.toggle('active');
+  body.classList.toggle('lock');
+};
+
+menu_list.onclick = function() {
+  menu_button.classList.toggle('active');
+  menu_itself.classList.toggle('active');
+  body.classList.toggle('lock');
+};
+
+
+
